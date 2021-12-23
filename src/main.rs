80a8ccafe8886ba359485aa27aefc12ff8c0f5c8
Jakob Rath <git@jakobrath.eu>
2021-12-23 11:15:35 +0100

    Read xlsx file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::path::PathBuf;
 
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use clap::Parser;
 use chrono::NaiveDate;
+use calamine::{Xlsx, Reader, open_workbook};
 
 #[derive(Debug, Parser)]
 #[clap(version, about)]
@@ -21,6 +22,18 @@ fn main() -> Result<()> {
 
     #[cfg(debug_assertions)]
     eprintln!("Options: {:?}", &opts);
+
+    let mut excel: Xlsx<_> = open_workbook(opts.file)?;
+
+    let sheet_name = "zebra";
+
+    let sheet =
+        excel.worksheet_range(sheet_name)
+        .ok_or_else(|| anyhow!("worksheet '{}' does not exist", sheet_name))??;
+
+    for row in sheet.rows() {
+        println!("row={:?}", row);
+    }
 
     Ok(())
 }
