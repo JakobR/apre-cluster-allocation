6e44fcefbc9c1adc36d55af1fbc27658ec656c30
Jakob Rath <git@jakobrath.eu>
2021-12-23 11:09:22 +0100

    Parse options

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::PathBuf;
+
+use anyhow::Result;
+use clap::Parser;
+use chrono::NaiveDate;
+
+#[derive(Debug, Parser)]
+#[clap(version, about)]
+struct Options {
+    /// Print allocation for the given date.
+    #[clap(short, long)]
+    date: Option<NaiveDate>,
+
+    /// The spreadsheet to parse.
+    /// The format should be *.xlsx.
+    file: PathBuf,
+}
+
+fn main() -> Result<()> {
+    let opts = Options::parse();
+
+    #[cfg(debug_assertions)]
+    eprintln!("Options: {:?}", &opts);
+
+    Ok(())
 }
