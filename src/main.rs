f53cf57541fee0899be5fb08a229c2323ff9c5b4
Jakob Rath <git@jakobrath.eu>
2022-01-11 17:44:05 +0100

    Print allocation; read date column directly

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::path::PathBuf;
 
 use anyhow::{anyhow, Result};
 use clap::Parser;
-use chrono::{Local, NaiveDate};
+use chrono::{Local, NaiveDate, Datelike};
 use calamine::{Xlsx, Reader, open_workbook, DataType};
 
 #[derive(Debug, Parser)]
@@ -16,6 +16,12 @@ struct Options {
     /// The spreadsheet to parse.
     /// The format should be *.xlsx.
     file: PathBuf,
+}
+
+#[derive(Debug, Clone)]
+struct Node {
+    name: String,
+    col: usize,
 }
 
 fn main() -> Result<()> {
@@ -44,20 +50,13 @@ fn main() -> Result<()> {
     let header = rows.next()
         .ok_or_else(|| anyhow!("no rows"))?;
 
-    let column_day = 7;
-    let column_month = 8;
-    let column_year = 9;
-    expect_header(header, column_day, "Day")?;
-    expect_header(header, column_month, "Month")?;
-    expect_header(header, column_year, "Year")?;
+    let col_date = 0;
+    expect_header(header, col_date, "Date")?;
 
     let mut selected_row = None;
 
     for row in rows {
-        let year = get_number(row, column_year)?.try_into()?;
-        let month = get_number(row, column_month)?.try_into()?;
-        let day = get_number(row, column_day)?.try_into()?;
-        let row_date = NaiveDate::from_ymd(year, month, day);
+        let row_date = get_as_date(row, col_date)?;
         if row_date == date {
             if selected_row.is_some() {
                 return Err(anyhow!("Multiple rows match the requested date '{}':\n{:?}\n{:?}", date, selected_row.unwrap(), row));
@@ -69,7 +68,22 @@ fn main() -> Result<()> {
     let selected_row = selected_row
         .ok_or_else(|| anyhow!("No row matches the requested date '{}'", date))?;
 
-    println!("row={:?}", selected_row);
+    #[cfg(debug_assertions)]
+    dbg!(selected_row);
+
+    let nodes = [
+        Node { name: "zebra-node01".into(), col: 4 },
+        Node { name: "zebra-node02".into(), col: 5 },
+        Node { name: "zebra-node03".into(), col: 6 },
+    ];
+
+    println!("Node allocation for {} ({}):", date, date.weekday());
+    for node in &nodes {
+        expect_header(header, node.col, &node.name)?;
+        let user = get_as_string(selected_row, node.col)?;
+        let user_display = if user.is_empty() { "<unassigned>" } else { &user };
+        println!("    {}: {}", node.name, user_display);
+    }
 
     Ok(())
 }
@@ -79,7 +93,7 @@ fn get_string(row: &[DataType], col: usize) -> Result<&str> {
         .get(col)
         .ok_or_else(|| anyhow!("no column of index {}", col))?
         .get_string()
-        .ok_or_else(|| anyhow!("expected String of column {}", col))
+        .ok_or_else(|| anyhow!("expected String in column {}", col))
 }
 
 fn expect_header(header: &[DataType], col: usize, text: &str) -> Result<()> {
@@ -90,6 +104,7 @@ fn expect_header(header: &[DataType], col: usize, text: &str) -> Result<()> {
     }
 }
 
+#[allow(dead_code)]
 fn get_number(row: &[DataType], col: usize) -> Result<i64> {
     let value =
         row.get(col)
@@ -97,6 +112,28 @@ fn get_number(row: &[DataType], col: usize) -> Result<i64> {
     match value {
         &DataType::Int(x) => Ok(x),
         &DataType::Float(x) => Ok(x as i64),
-        x => Err(anyhow!("Expected number in column '{}', but got '{}'", col, x)),
+        x => Err(anyhow!("expected number in column {}, but got '{}'", col, x)),
+    }
+}
+
+fn get_as_date(row: &[DataType], col: usize) -> Result<NaiveDate> {
+    row.get(col)
+        .ok_or_else(|| anyhow!("no column of index {}", col))?
+        .as_date()
+        .ok_or_else(|| anyhow!("expected date in column {}", col))
+}
+
+fn get_as_string(row: &[DataType], col: usize) -> Result<String> {
+    let value =
+        row.get(col)
+        .ok_or_else(|| anyhow!("no column of index {}", col))?;
+    match value {
+        &DataType::Bool(x) => Ok(format!("{}", x)),
+        &DataType::Int(x) => Ok(format!("{}", x)),
+        &DataType::Float(x) => Ok(format!("{}", x)),
+        &DataType::DateTime(x) => Ok(format!("{}", x)),
+        &DataType::String(ref x) => Ok(x.clone()),
+        &DataType::Empty => Ok(String::new()),
+        &DataType::Error(ref x) => Ok(format!("#ERROR: {}", x)),
     }
 }
