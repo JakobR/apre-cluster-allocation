7030901fd8353d36828a32dbbbdeb94658dc9ebe
Jakob Rath <git@jakobrath.eu>
2022-01-14 07:52:15 +0100

    Try the new formatting strings

--- src/main.rs
+++ src/main.rs
@@ -43,7 +43,7 @@ fn main() -> Result<()> {
 
     let sheet =
         excel.worksheet_range(sheet_name)
-        .ok_or_else(|| anyhow!("worksheet '{}' does not exist", sheet_name))??;
+        .ok_or_else(|| anyhow!("worksheet '{sheet_name}' does not exist"))??;
 
     let mut rows = sheet.rows();
 
@@ -59,14 +59,14 @@ fn main() -> Result<()> {
         let row_date = get_as_date(row, col_date)?;
         if row_date == date {
             if selected_row.is_some() {
-                return Err(anyhow!("Multiple rows match the requested date '{}':\n{:?}\n{:?}", date, selected_row.unwrap(), row));
+                return Err(anyhow!("Multiple rows match the requested date '{date}':\n{0:?}\n{row:?}", selected_row.unwrap()));
             }
             selected_row = Some(row);
         }
     }
 
     let selected_row = selected_row
-        .ok_or_else(|| anyhow!("No row matches the requested date '{}'", date))?;
+        .ok_or_else(|| anyhow!("No row matches the requested date '{date}'"))?;
 
     #[cfg(debug_assertions)]
     dbg!(selected_row);
@@ -91,14 +91,14 @@ fn main() -> Result<()> {
 fn get_string(row: &[DataType], col: usize) -> Result<&str> {
     row
         .get(col)
-        .ok_or_else(|| anyhow!("no column of index {}", col))?
+        .ok_or_else(|| anyhow!("no column of index {col}"))?
         .get_string()
-        .ok_or_else(|| anyhow!("expected String in column {}", col))
+        .ok_or_else(|| anyhow!("expected String in column {col}"))
 }
 
 fn expect_header(header: &[DataType], col: usize, text: &str) -> Result<()> {
     if get_string(header, col)? != text {
-        Err(anyhow!("Expected header '{}' in column '{}'", text, col))
+        Err(anyhow!("Expected header '{text}' in column '{col}'"))
     } else {
         Ok(())
     }
@@ -108,32 +108,32 @@ fn expect_header(header: &[DataType], col: usize, text: &str) -> Result<()> {
 fn get_number(row: &[DataType], col: usize) -> Result<i64> {
     let value =
         row.get(col)
-        .ok_or_else(|| anyhow!("no column of index {}", col))?;
+        .ok_or_else(|| anyhow!("no column of index {col}"))?;
     match value {
         &DataType::Int(x) => Ok(x),
         &DataType::Float(x) => Ok(x as i64),
-        x => Err(anyhow!("expected number in column {}, but got '{}'", col, x)),
+        x => Err(anyhow!("expected number in column {col}, but got '{x}'")),
     }
 }
 
 fn get_as_date(row: &[DataType], col: usize) -> Result<NaiveDate> {
     row.get(col)
-        .ok_or_else(|| anyhow!("no column of index {}", col))?
+        .ok_or_else(|| anyhow!("no column of index {col}"))?
         .as_date()
-        .ok_or_else(|| anyhow!("expected date in column {}", col))
+        .ok_or_else(|| anyhow!("expected date in column {col}"))
 }
 
 fn get_as_string(row: &[DataType], col: usize) -> Result<String> {
     let value =
         row.get(col)
-        .ok_or_else(|| anyhow!("no column of index {}", col))?;
+        .ok_or_else(|| anyhow!("no column of index {col}"))?;
     match value {
-        &DataType::Bool(x) => Ok(format!("{}", x)),
-        &DataType::Int(x) => Ok(format!("{}", x)),
-        &DataType::Float(x) => Ok(format!("{}", x)),
-        &DataType::DateTime(x) => Ok(format!("{}", x)),
+        &DataType::Bool(x) => Ok(format!("{x}")),
+        &DataType::Int(x) => Ok(format!("{x}")),
+        &DataType::Float(x) => Ok(format!("{x}")),
+        &DataType::DateTime(x) => Ok(format!("{x}")),
         &DataType::String(ref x) => Ok(x.clone()),
         &DataType::Empty => Ok(String::new()),
-        &DataType::Error(ref x) => Ok(format!("#ERROR: {}", x)),
+        &DataType::Error(ref x) => Ok(format!("#ERROR: {x}")),
     }
 }
